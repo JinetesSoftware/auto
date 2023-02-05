62748865bb4e785fc5b44dc96305d91404403e9e
LJAA1994 <lucas.teleco.info@gmail.com>
2023-02-05 20:14:36 +0000

    feat(clients): render list of clients

--- front/src/app/modules/main/pages/clients/clients.module.ts
+++ front/src/app/modules/main/pages/clients/clients.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
-import { ClientsComponent } from './clients.component';
+import { ClientsComponent } from './features/clients/clients.component';
 import { NewClientComponent } from './features/new-client/new-client.component';
 import { WorkOrdersComponent } from './features/work-orders/work-orders.component';
 import { BillingComponent } from './features/billing/billing.component';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule } from 'src/app/shared/modules/components/components.module';
+
 
 @NgModule({
   declarations: [
--- front/src/app/modules/main/pages/clients/features/clients/clients.component.ts
+++ front/src/app/modules/main/pages/clients/features/clients/clients.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs';
+import { Client } from 'src/app/core/models/Client';
 import { ClientsService } from 'src/app/modules/main/services/clients.service';
 
 @Component({
@@ -8,14 +9,20 @@ import { ClientsService } from 'src/app/modules/main/services/clients.service';
   styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent {
+clients: Client[] = [];
+displayedColumns: any = ['person_name','email','phone_number']
+  constructor(private clientService: ClientsService) {
+    this.clientService.getClients().subscribe((resp) => {
+      if (resp) {
+        this.clients = resp.clients;
+        console.log('Clientes',this.clients);
+      }
+    })
+   }
 
-  constructor(private c: ClientsService) { }
-  testData!: string;
 
   testReq() {
-    this.c.testReq().pipe(map(res => res.msg)).subscribe(res => {
-      this.testData = res;
-    });
+
 
   }
 
